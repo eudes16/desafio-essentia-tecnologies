b44969cad6c8d6ce40db20c0fac663bcd60cf119
Eudes Vicente de Souza Silva <eudes.vss@gmail.com>
2025-06-18 21:43:25 -0300

    Adicionando soft delete middleware no prisma

--- api/src/shared/db/prismaClient.ts
+++ api/src/shared/db/prismaClient.ts
@@ -1,9 +1,27 @@
 import { PrismaClient } from "../../generated/client"
+import resolveParams from "../../http/resolveParams"
 
 const log = (process.env.ERROR_LOG?.replace(/\s/g, "")?.split(',') || []) as any
 
 const prismaClient = new PrismaClient({
     log: log
-})
+}).$extends({
+    query: {
+        $allModels: {
+            async $allOperations({ model, operation, args, query }) {
+                if (
+                    operation === "findMany" || operation === "aggregate" || operation === "count" || operation === "findFirst" 
+                    || operation === "findUnique" || operation === "findFirstOrThrow" || operation === "findUniqueOrThrow"
+                ) {
+                    args.where = {
+                        ...args.where,
+                        deletedAt: null,
+                    }
+                }
+                return query(args)
+            },
+        },
+    },
+});
 
 export default prismaClient
