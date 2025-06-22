0678cb02d1678a9da268ade60b79ac8ac87cf072
Eudes Vicente de Souza Silva <eudes.vss@gmail.com>
2025-06-22 08:55:42 -0300

    Adicionado PrimeNg como provedor do tema alterando a estratégia inicial com TailwindCSS

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,19 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment.development';
-import { Header } from "./shared/components/layout/header/header";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCalendar } from '@fortawesome/free-regular-svg-icons';
-
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Header, FontAwesomeModule],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
-  providers: []
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.scss'
 })
 export class App {
+    protected title = 'app';
 
 }
--- app/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://www.local.api.com:4000/api',
-    appName: 'Essentia Todo App',
-    appVersion: '1.0.0'
-};
